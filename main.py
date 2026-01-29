edd20fbb23d40c28f3b8352453b890d205b51f4b
Davronbekn202 <davronbekn202@gmail.com>
2026-01-29 12:30:41 +0500

    update 1

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from datetime import datetime
 from information import *
+
+
 def insert_to_users_main():
     full_name = input("Enter full-name: ")
     phone = int(input("Enter phone: "))
     password = input("Enter password: ")
     role = input("Enter your role: ")
     insert_to_users(full_name, phone, password, role)
+
+
 def categories_main():
     name = input("Enter your name: ")
     categories(name)
+
+
 def products_main():
     name = input("Enter your name: ")
     description = input("Enter description: ")
@@ -16,26 +22,36 @@ def products_main():
     stock = int(input("Enter the stock: "))
     category_id = int(input("Enter category-id: "))
     products(name, description, price, stock, category_id)
-def carts():
+
+
+def carts_main():
     carts_id = int(input("Enter your carts-id: "))
     carts(carts_id)
+
+
 def cart_items_main():
     carts_id = int(input("Enter your carts-id: "))
     product_id = int(input("Enter your product-id: "))
     quantity = int(input("Enter the quantity: "))
     cart_items(carts_id, product_id, quantity)
-def orders():
+
+
+def orders_main():
     user_id = int(input("Enter your user-id: "))
     total_price = float(input("Enter total price: "))
     status = input("Enter status: ")
     orders(user_id, total_price, status)
-def order_items():
+
+
+def order_items_main():
     order_id = int(input("Enter your order-id: "))
     product_id = int(input("Enter product-id"))
     quantity = int(input("Enter quantity: "))
     price = float(input("Enter the price: "))
     order_items(order_id, product_id, quantity, price)
-def payments():
+
+
+def payments_main():
     order_id = int(input("Enter id of order: "))
     product_id = int(input("Enter id of product: "))
     amount = float(input("Enter the amount: "))
@@ -43,7 +59,8 @@ def payments():
     paid_at = datetime.now()
     payments(order_id, product_id, amount, status, paid_at)
 
-def reviews():
+
+def reviews_main():
     product_id = int(input("Enter your user-id: "))
     user_id = int(input("Enter your user-id: "))
     rating = int(input("try to rate: "))
--- take_info.py
+++ take_info.py
@@ -1,7 +1,34 @@
-from information import *
+from main import *
 
+print("""
+1 - Admin
+2 - User
+""")
+secret_key_admin = 'a102938b'
+check = int(input("choose: "))
+if check == 1:
+    for i in range(10):
+        cut = input("Enter secret-key of admins: ")
+        if cut == secret_key_admin:
+            print("Welcome Admin")
+        else:
+            print("You are not admin")
+elif check == 2:
+    print("""
+    1 - add user
+    2 - payment
+    """)
+    choice = int(input())
+    if choice == 1:
+        insert_to_users_main()
+    elif choice == 2:
+        payments_main()
 
-
-
-
-
+else:
+    try:
+        if type(check) == str:
+            raise TypeError("type have to be int.")
+        else:
+            pass
+    except Exception as x:
+        print(x)
