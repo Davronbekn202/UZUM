e3c0c484258e4b51f8dac41b2c4428169670bb57
Davronbekn202 <davronbekn202@gmail.com>
2026-02-01 01:06:36 +0500

    first view created

--- view.py
+++ view.py
@@ -9,4 +9,26 @@ def see_info():
     user_info_fetched = cursor.fetchall()
     for i in user_info_fetched:
         print(i)
+
+def make_view():
+    connect = get_connection()
+    cursor = connect.cursor()   # 👈 parentheses matter
+
+    cursor.execute("""
+        CREATE VIEW as_use AS
+        SELECT
+            u.id,
+            u.full_name,
+            o.total_price
+        FROM users u
+        INNER JOIN orders o
+            ON u.id = o.user_id;
+    """)
+
+    connect.commit()
+    cursor.close()
+    connect.close()
+    print("as_use created")
+
+
 see_info()
